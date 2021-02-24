bf1ae85996ffb4ff6209b05290de2da74457da5e
Kawian <99052760@mydavinci.nl>
2021-02-24 15:33:10 +0100

    3 levels toegevoegd en bezig met inventory item

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 
-var container = document.getElementById("game-container");
-var title = document.getElementById("title");
-var description = document.getElementById("description");
-var gameButtons = document.getElementById("game-buttons");
-var button1 = document.getElementById("button1");
-var button2 = document.getElementById("button2");
+const container = document.getElementById("game-container");
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const gameButtons = document.getElementById("game-buttons");
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const inventory = document.getElementById("inventoryItem");
+var sword = false;
 
 
 
@@ -20,7 +22,7 @@ function start(){
     button1.innerHTML = "Start";
     button2.innerHTML = "Audio";
     button1.onclick = onbutton1clicked;
-    button2.onclick = audio;
+    button2.onclick = music;
 }
 
 function onbutton1clicked(){
@@ -32,4 +34,70 @@ function onbutton1clicked(){
     button2.innerHTML = "Weiger";
     button1.onclick = strijden;
     button2.onclick = weiger;
+}
+
+function strijden(){
+	container.style.backgroundImage = "url('images/b892326be286e88083f3c025c92edef5.png')";
+	title.innerHTML = "De reis";
+	title.style.color = "black";
+	console.log("De reis");
+	description.innerHTML = "Je trekt ten strijden tegen de Vikingen. Na drie lange dagen zijn jullie eindelijk in het gebied van de vikingen aangekomen. Jullie bespreken de tactiek, maar de ene helft van de troepen wilt overdag via de poort aanvallen en elke Viking afslachten die ze tegenkomen. Maar de andere helft wil 's nachts het fort binnen dringen als een soort verrassinges aanval. De leider vraagt het aan jou. Het hangt er allemaal af van wat jij kiest. Wat word het? Overdag of 's nachts";
+	description.style.color = "white";
+	button1.innerHTML = "overdag";
+	button2.innerHTML = "nacht";
+	button1.onclick = overdag;
+	button2.onclick = nacht;
+}
+
+function overdag(){
+	container.style.backgroundImage = "url('images/army.jpg')";
+	title.innerHTML = "vechten voor de poort";
+	title.style.color = "white";
+	console.log("Vechten voor de poort");
+	description.innerHTML = "Het was jou keuze dus dat respecteerd iedereen. Jullie stormen op de poort af en breken hem open. Het is een hevige strijd. Je verslaat een Vinking met een hoge rang. Hij dropt zn zwaard, wil je deze oppakken of laten liggen?";
+	description.style.color = "white";
+	button1.innerHTML = "oppakken";
+	button2.innerHTML = "doorgaan";
+	button1.onclick = function() {
+		Zwaard();
+		sword = true;
+		console.log(sword);
+	}
+	button2.onclick = doorgaan;
+}
+
+function Zwaard() {
+	container.style.backgroundImage = "none";
+	container.style.backgroundColor= "black";
+	description.style.display = "none";
+	title.style.color = "black";
+
+	var swordImage = document.createElement("img");
+	container.appendChild(swordImage);
+	swordImage.src = "images/zwaard.jpg";
+	swordImage.style.display = "block";
+	swordImage.style.margin = "0 auto";
+
+	swordImage.onclick = function(){
+		duel();
+		console.log(inventory);
+		swordImage.style.display = "none";
+		inventory.style.display = "block";
+		inventory.src = "images/zwaard.jpg";
+	};
+	button1.style.display = "none";
+	button2.style.display = "none";
+}
+
+function doorgaan(){
+	container.style.backgroundImage = "url('images/samuraiFight.jpg')";
+	title.innerHTML = "vechten voor de poort";
+	console.log("Doorgaan");
+	title.style.color = "black";
+	description.innerHTML = "Je laat het zwaard liggen en vecht je weg door het fort totdat de leider van de Vikingen je naam roept en je uitdaagd voor een duel. Ga je dit dual aan of niet?";
+	description.style.color = "white";
+	button1.innerHTML = "duel";
+	button2.innerHTML = "geen duel";
+	button1.onclick = DUEL;
+	button2.onclick = GEENduel;
 }
